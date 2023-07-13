8ff502a24106199606de7ff1ab7e79d70b39324c
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-07-14 04:32:12 +0800

    use send message to test

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,19 +1,23 @@
-const version = 'v4'
+const version = 'v1'
 
 const cacheFirstFiles = [
   '/favicon_package/apple-touch-icon.png',
   '/favicon_package/favicon-32x32.png',
   '/favicon_package/favicon-16x16.png',
-  '/favicon_package/safari-pinned-tab.svg'
+  '/favicon_package/safari-pinned-tab.svg',
+  '/favicon_package/android-chrome-192x192.png',
+  // '/assignment/images/cambodia.jpg',
+  // '/assignment/images/korea.jpg',
+  // '/assignment/images/exotic.jpg',
 ]
 
 const networkFirstFiles = [
-  'index.html',
-  'marker.html',
-  'package.html',
-  'style.css',
-  'manifest.json',
-  'loadSW.js'
+  '/index.html',
+  '/marker.html',
+  '/package.html',
+  '/style.css',
+  '/manifest.json',
+  '/loadSW.js'
 ]
 
 const cacheFiles = cacheFirstFiles.concat(networkFirstFiles)
@@ -40,24 +44,25 @@ self.addEventListener('activate', (event) => {
   )
 })
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request)
-    })
-  )
-})
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       return response || fetch(event.request)
+//     })
+//   )
+// })
 
-// self.addEventListener('fetch', event => {
-//   if (event.request.method !== 'GET') { return }
-//   if (networkFirstFiles.indexOf(event.request.url) !== -1) {
-//     event.respondWith(networkElseCache(event));
-//   } else if (cacheFirstFiles.indexOf(event.request.url) !== -1) {
-//     event.respondWith(cacheElseNetwork(event));
-//   } else {
-//     event.respondWith(fetch(event.request));
-//   }
-// });
+self.addEventListener('fetch', event => {
+  const pathname = new URL(event.request.url).pathname
+  if (event.request.method !== 'GET') { return }
+  if (networkFirstFiles.indexOf(pathname) !== -1) {
+    event.respondWith(networkElseCache(event))
+  } else if (cacheFirstFiles.indexOf(pathname) !== -1) {
+    event.respondWith(cacheElseNetwork(event))
+  } else {
+    event.respondWith(fetch(event.request))
+  }
+});
 
 
 self.addEventListener('push', (event) => {
@@ -90,20 +95,55 @@ function cacheElseNetwork(event) {
 }
 
 function networkElseCache(event) {
-  const response = fetchAndCache(event)
-  return response.ok ? response : caches.match(event.request)
+  const pathname = new URL(event.request.url).pathname
+  // caches.match(event.request).then(response => {
+  //   send_message_to_all_clients(response)
+  // }).catch(() => {
+  //   send_message_to_all_clients('nothinbh')
+  // }).finally(() => {
+  //   send_message_to_all_clients('nothinbh')
+  // })
+  
+  const response = fetchAndCache(event).then(res => {
+    // send_message_to_all_clients('result')
+  })
+  const result = response.ok ? response : caches.match(event.request)
+  send_message_to_all_clients(result)
+  return result
 }
 
-function fetchAndCache(event) {
-  fetch(event.request).then(response => {
+async function fetchAndCache(event) {
+  const pathname = new URL(event.request.url).pathname
+  send_message_to_all_clients(pathname)
+  return await fetch(event.request).then(response => {
+    send_message_to_all_clients(pathname + ': ' +response.ok)
     if (response.ok) {
       caches.open(version).then(cache => {
         cache.put(event.request, response.clone())
       })
     }
     return response
+  }).catch(() => {
+    send_message_to_all_clients('nothinbh')
   })
 }
+
+// function getCleanRequest(request) {
+//   const url = new URL(request.url);
+//   url.search = ''
+//   url.fragment = ''
+
+//   let cleanRequest = new Request(url, {
+//     method: request.method,
+//     headers: request.headers,
+//     mode: request.mode,
+//     credentials: request.credentials,
+//     cache: request.cache,
+//     redirect: request.redirect,
+//     referrer: request.referrer,
+//     integrity: request.integrity,
+//   });
+// }
 
 function send_message_to_client(client, msg) {
   return new Promise((resolve, reject) => {
